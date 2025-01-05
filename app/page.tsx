d5746a0268b5638067a66f58187d27698e54d9c4
Ehis Ojielu <eojielu@gmail.com>
2025-01-04 23:17:14 -0600

    feat: add more marketing copy

--- app/page.tsx
+++ app/page.tsx
@@ -16,10 +16,31 @@ export default function Home() {
       </div>
       <div className="text-white flex md:flex-row gap-4 self-end w-full md:w-9/12">
         <Rotate bg="#1535c4" height="auto" />
-        <div className="p-8 text-3xl md:text-6xl bg-zinc-800">
+        <div
+          className="p-8 text-3xl md:text-6xl"
+          style={{ backgroundColor: "#828282" }}
+        >
           /wāv/ a music collaboration and workflow company
         </div>
         <Rotate bg="#ffd917" height="auto" />
+      </div>
+      <div className="text-white flex md:flex-row gap-4 self-start w-full md:w-9/12">
+        <Rotate bg="#ff5a00" height="auto" />
+        <div className="p-8 text-3xl md:text-6xl">
+          an easy way to upload, and share your creations with your circle.
+        </div>
+        <Rotate bg="#e7e7e7" height="auto" />
+      </div>
+      <div className="text-white flex md:flex-row gap-4 self-end w-full md:w-9/12">
+        <Rotate bg="#f73375" height="auto" />
+        <div
+          className="p-8 text-3xl md:text-6xl"
+          style={{ backgroundColor: "#828282" }}
+        >
+          collaborate with your team and keep the conversation going in a single
+          place.
+        </div>
+        <Rotate bg="#ff5a00" height="auto" />
       </div>
       <footer className="self-start text-white text-3xl">
         info@itswave.app
